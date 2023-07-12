9c8c3a4ccf1f726021d57f6e7d33e35179e0d963
Wangari <109669003+karreycodes@users.noreply.github.com>
2023-07-12 16:11:09 +0300

    markdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -1,46 +1,50 @@
 #!/usr/bin/python3
+"""
+A script that converts Markdown to HTML.
+"""
+
 import sys
 import os
-import markdown
-import hashlib
 import re
 
-def parse_md5(match):
-    content = match.group(1)
-    md5_hash = hashlib.md5(content.encode()).hexdigest()
-    return md5_hash
+def convert_markdown_to_html(input_file, output_file):
+    """
+    Converts a Markdown file to HTML and writes the output to a file.
+    """
+    # Check that the Markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
 
-def parse_removal(match):
-    content = match.group(1)
-    modified_content = re.sub(r'[cC]', '', content)
-    return modified_content
+    # Read the Markdown file and convert it to HTML
+    with open(input_file, encoding="utf-8") as f:
+        html_lines = []
+        for line in f:
+            # Check for Markdown headings
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                heading_level = len(match.group(1))
+                heading_text = match.group(2)
+                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                html_lines.append(line.rstrip())
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
-    sys.exit(1)
+    # Write the HTML output to a file
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(html_lines))
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+if __name__ == "__main__":
+    # Check that the correct number of arguments were provided
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
 
-if not os.path.isfile(input_file):
-    print(f"Missing {input_file}", file=sys.stderr)
-    sys.exit(1)
+    # Get the input and output file names from the command-line arguments
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-with open(input_file, "r") as f:
-    markdown_text = f.read()
+    # Convert the Markdown file to HTML and write the output to a file
+    convert_markdown_to_html(input_file, output_file)
 
-html = markdown.markdown(
-    markdown_text,
-    extensions=["markdown.extensions.extra", "markdown.extensions.nl2br"]
-)
-
-# Parse custom syntax for MD5 conversion
-html = re.sub(r"\[\[(.*?)\]\]", parse_md5, html)
-
-# Parse custom syntax for removing characters
-html = re.sub(r"\(\((.*?)\)\)", parse_removal, html, flags=re.IGNORECASE)
-
-with open(output_file, "w") as f:
-    f.write(html)
-
-sys.exit(0)
+    # Exit with a successful status code
+    sys.exit(0)
